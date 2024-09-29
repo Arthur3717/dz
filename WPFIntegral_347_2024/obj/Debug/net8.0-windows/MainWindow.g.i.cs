b656bf084f771b178881ca2ba8df86ea69a8bec8
Arthur <arthur.xx2003@gmail.com>
2024-09-30 02:12:40 +0300

    вторая версия

--- WPFIntegral_347_2024/obj/Debug/net8.0-windows/MainWindow.g.i.cs
+++ WPFIntegral_347_2024/obj/Debug/net8.0-windows/MainWindow.g.i.cs
@@ -101,7 +101,7 @@ namespace WPFIntegral_347_2024 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WPFIntegral_347_2024;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WPFIntegral_347_2024;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
